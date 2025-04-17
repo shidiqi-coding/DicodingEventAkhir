3637172fea62238533656a4da095b3ff84f737f0
shidiqi-coding <shidiqiihsan@gmail.com>
2025-04-17 10:08:04 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
 
     implementation("com.google.android.material:material:1.11.0")
 }
